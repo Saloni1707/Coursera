6694ce06b579bcc8336cf18a9b6ea843ba8a740f
Saloni Atole <145243379+Saloni1707@users.noreply.github.com>
2024-09-27 21:24:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
+require ("dotenv").config();
 const express = require ("express");
 const mongoose  = require("mongoose");
 
 const app = express();
 const { userRouter } = require("./routes/user");
-const { userCourse } = require("./routes/course");
+const { CourseRouter  } = require("./routes/course");
 const {adminrouter} = require("./routes/admin")
 
 // createCourseRoutes(app);       
 // createUserRoutes(app);      
 
 app.use("/api/v1/user",userRouter);   
-app.use("/api/v1/course",userCourse);
+app.use("/api/v1/course",CourseRouter);
 app.use("/api/v1/admin",adminrouter);
 async function main(){ 
     //use dotend to put ur environment variables ka (string) in another file
-    await mongoose.connect("mongodb+srv://SaloniAtole:Qs72aKQ7eaZHkiuF@cluster0.po79z.mongodb.net/Coursera-app");
+    await mongoose.connect(process.env.MONGO_URL);
     app.listen(3000);
     console.log("Listening on the port ");
 
